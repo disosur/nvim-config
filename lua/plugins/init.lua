a4457cdf04764947f7b7d4728895c329be709701
Alyssair Renhee Nikolai Morales <alymond.business@outlook.com>
2024-06-13 15:30:49 +0800

    Official Initial Commit

---
+++ lua/plugins/init.lua
@@ -0,0 +1,36 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    config = function()
+      require "configs.conform"
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
+  },
+  
+  {
+  	"williamboman/mason.nvim",
+  	opts = {
+  		ensure_installed = {
+  			"lua-language-server", "stylua", "tailwind-language-server", "gopls", 
+  			"html-lsp", "css-lsp" , "prettierd", "typescript-language-server",
+  		},
+  	},
+  },
+  
+  {
+  	"nvim-treesitter/nvim-treesitter",
+  	opts = {
+  		ensure_installed = {
+  			"vim", "lua", "vimdoc",
+       "html", "css", "jsx", javascript, "typescript", "tsx",
+  		},
+  	},
+  },
+}
