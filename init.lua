b649480c5af37309f81c5b9517eab02a47cbcff0
Alyssair Renhee Nikolai Morales <alymond.business@outlook.com>
2024-07-30 22:04:06 +0800

    updated config to work with templ

--- init.lua
+++ init.lua
@@ -38,3 +38,25 @@ require "nvchad.autocmds"
 vim.schedule(function()
   require "mappings"
 end)
+
+
+local templ_format = function()
+  local bufnr = vim.api.nvim_get_current_buf()
+  local filename = vim.api.nvim_buf_get_name(bufnr)
+  local cmd = "templ fmt " .. vim.fn.shellescape(filename)
+
+  vim.fn.jobstart(cmd, {
+    on_exit = function()
+      -- Reload the buffer only if it's still the current buffer
+      if vim.api.nvim_get_current_buf() == bufnr then
+        vim.cmd('e!')
+      end
+    end,
+  })
+end
+
+-- Create an autocmd to run templ_format on save for .templ files
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  pattern = { "*.templ" },
+  callback = templ_format
+})
