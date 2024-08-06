c0a134ff67ba08145529cf673041134ac8c9cee4
Alyssair Renhee Nikolai Morales <alymond.business@outlook.com>
2024-08-06 21:08:09 +0800

    Added svelte and python support

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -5,7 +5,7 @@ local capabilities = require("nvchad.configs.lspconfig").capabilities
 local lspconfig = require "lspconfig"
 local util = require "lspconfig/util"
 
-local servers = { "tsserver", "tailwindcss", "eslint" }
+local servers = { "tsserver", "tailwindcss", "eslint", "pyright", "ruff_lsp" }
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
