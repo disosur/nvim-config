d94d19125055e67927a3dcc212cb33b37d9165b7
Alysstair Morales <disosur@proton.me>
2024-10-10 22:34:36 +0800

    test commit

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -40,7 +40,7 @@ return {
     lazy = false,
     dependencies = {
       "nvim-lua/plenary.nvim",
-      "stevearc/dressing.nvim", -- optional for vim.ui.select
+      "stevearc/dressing.nvim",
     },
     config = true,
   },
